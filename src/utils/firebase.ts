f61120beef51c38adb71b91ed602f0899c9be464
Antonio Ribeiro <antonio.ribeiro@ntconsult.com.br>
2020-01-02 18:46:09 -0300

    removed config from file

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,16 +1,9 @@
 import firebase from 'firebase';
+const config = require('./config.json');
 
 export class Firebase {
   public static init() {
-    var firebaseConfig = {
-      apiKey: "AIzaSyCrD3JsZhpy1Vn_-WSh1IHGmqSu69f7RXM",
-      authDomain: "csr-web.firebaseapp.com",
-      databaseURL: "https://csr-web.firebaseio.com",
-      projectId: "csr-web",
-      storageBucket: "csr-web.appspot.com",
-      messagingSenderId: "301602435301",
-      appId: "1:301602435301:web:9b3b7e10b9eaae260b240c"
-    };
+    const firebaseConfig = config;
     firebase.initializeApp(firebaseConfig);
   }
 }
